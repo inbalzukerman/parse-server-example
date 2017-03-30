cef773056c5c8b34cba5b16faf24fcb3286ff6fc
inbalzukerman <inbal.zukerman@gmail.com>
2017-03-30 14:35:18 +0300

    update index.js

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_gnpks1lz:kdk8clh3ri34oj7h0am6ao9pbi@ds145230.mlab.com:45230/heroku_gnpks1lz
+',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'smartcity-smarthouse',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://smartcity-smarthouse.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
